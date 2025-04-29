aeb32e735764e15e5f88a17296d057162bfc868f
Jossi Wolf <jossiwolf@google.com>
2025-04-29 15:52:01 +0100

    Use LibraryGlideModule for Pokedex-Compose
    To avoid clashes with Pokedex-Views, both are LibraryGlideModule now. The GlideAppModule is moving out to the macrobenchmark-target.
    
    Test: Ran benchmark against target
    Change-Id: Id3e6f2ba15d3381d1c6c10b79436717bf07769d6

--- app/src/main/kotlin/com/skydoves/pokedex/compose/ui/PokedexComposeGlideAppModule.kt
+++ app/src/main/kotlin/com/skydoves/pokedex/compose/ui/PokedexComposeGlideAppModule.kt
@@ -22,12 +22,12 @@ import com.bumptech.glide.Registry
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
 import com.bumptech.glide.load.model.GlideUrl
-import com.bumptech.glide.module.AppGlideModule
+import com.bumptech.glide.module.LibraryGlideModule
 import com.skydoves.pokedex.compose.core.network.di.ModuleLocator
 import java.io.InputStream
 
 @GlideModule
-class PokedexGlideAppModule : AppGlideModule() {
+class PokedexComposeGlideAppModule : LibraryGlideModule() {
     override fun registerComponents(context: Context, glide: Glide, registry: Registry) {
         val okHttpClient = ModuleLocator.networkModule.okHttpClientFactory()
         val loaderFactory = OkHttpUrlLoader.Factory(okHttpClient)
